8a9ac1b15ea0a0fc20eecbf49a5170161ef07bf9
thecuongk5hp <147390984+thecuongk5hp@users.noreply.github.com>
2025-03-10 15:42:31 +0700

    Update Tt1.java

--- src/Tt1.java
+++ src/Tt1.java
@@ -85,89 +85,101 @@ public class Tt1 {
                     }
                     System.out.println("Số lượng số nguyên tố: " + primeCount);
                     break;
+     case 7:
+                    int evenCount = 0, oddCount = 0, otherCount = 0;
 
-                case 7:
-                    int[] a = new int[n];
-                    int[] b = new int[n];
-                    int[] c = new int[n];
-                    int aSize = 0, bSize = 0, cSize = 0;
-
-                    for (int i = 0; i < n; i++) {
-                        if (arr[i] % 3 == 0) {
-                            if (arr[i] % 2 == 0) {
-                                a[aSize] = arr[i];
-                                aSize++;
-                            } else {
-                                c[cSize] = arr[i];
-                                cSize++;
-                            }
-                        } else {
-                            b[bSize] = arr[i];
-                            bSize++;
-                        }
+                    for (int num : numbers) {
+                        if (num % 2 == 0 && num % 3 == 0) evenCount++;
+                        else if (num % 2 != 0 && num % 3 == 0) oddCount++;
+                        else otherCount++;
                     }
 
-                    for (int i = 0; i < aSize - 1; i++) {
-                        for (int j = i + 1; j < aSize; j++) {
-                            if (a[i] > a[j]) {
-                                int temp = a[i];
-                                a[i] = a[j];
-                                a[j] = temp;
-                            }
-                        }
+                    int[] evenDivisibleBy3 = new int[evenCount];
+                    int[] oddDivisibleBy3 = new int[oddCount];
+                    int[] others = new int[otherCount];
+
+                    int evenIndex = 0, oddIndex = 0, otherIndex = 0;
+
+                    for (int num : numbers) {
+                        if (num % 2 == 0 && num % 3 == 0) evenDivisibleBy3[evenIndex++] = num;
+                        else if (num % 2 != 0 && num % 3 == 0) oddDivisibleBy3[oddIndex++] = num;
+                        else others[otherIndex++] = num;
                     }
 
-                    for (int i = 0; i < bSize - 1; i++) {
-                        for (int j = i + 1; j < bSize; j++) {
-                            if (b[i] > b[j]) {
-                                int temp = b[i];
-                                b[i] = b[j];
-                                b[j] = temp;
+                    int n = evenDivisibleBy3.length;
+                    for (int i = 0; i < n - 1; i++) {
+                        for (int j = 0; j < n - i - 1; j++) {
+                            if (evenDivisibleBy3[j] > evenDivisibleBy3[j + 1]) {
+                                int temp = evenDivisibleBy3[j];
+                                evenDivisibleBy3[j] = evenDivisibleBy3[j + 1];
+                                evenDivisibleBy3[j + 1] = temp;
                             }
                         }
                     }
 
-                    for (int i = 0; i < cSize - 1; i++) {
-                        for (int j = i + 1; j < cSize; j++) {
-                            if (c[i] < c[j]) {
-                                int temp = c[i];
-                                c[i] = c[j];
-                                c[j] = temp;
+                    n = others.length;
+                    for (int i = 0; i < n - 1; i++) {
+                        for (int j = 0; j < n - i - 1; j++) {
+                            if (others[j] > others[j + 1]) {
+                                int temp = others[j];
+                                others[j] = others[j + 1];
+                                others[j + 1] = temp;
+                            }
+                        }
+                    }
+
+                    n = oddDivisibleBy3.length;
+                    for (int i = 0; i < n - 1; i++) {
+                        for (int j = 0; j < n - i - 1; j++) {
+                            if (oddDivisibleBy3[j] < oddDivisibleBy3[j + 1]) {
+                                int temp = oddDivisibleBy3[j];
+                                oddDivisibleBy3[j] = oddDivisibleBy3[j + 1];
+                                oddDivisibleBy3[j + 1] = temp;
                             }
                         }
                     }
 
                     int index = 0;
-                    for (int i = 0; i < aSize; i++) {
-                        arr[index] = a[i];
-                        index++;
-                    }
-                    for (int i = 0; i < bSize; i++) {
-                        arr[index] = b[i];
-                        index++;
-                    }
-                    for (int i = 0; i < cSize; i++) {
-                        arr[index] = c[i];
-                        index++;
-                    }
-                    break;
+                    for (int num : evenDivisibleBy3) numbers[index++] = num;
+                    for (int num : others) numbers[index++] = num;
+                    for (int num : oddDivisibleBy3) numbers[index++] = num;
 
+                    System.out.print("Mảng sau khi sắp xếp: ");
+                    for (int num : numbers) {
+                        System.out.print(num + " ");
+                    }
+                    System.out.println();
+                    break;
                 case 8:
-                    System.out.print("Nhập m = ");
-                    int m = sc.nextInt();
-                    arr[n] = m;
-                    n++;
-                    for (int i = 0; i < n - 1; i++) {
-                        for (int j = 0; j < n - i - 1; j++) {
-                            if (arr[j] < arr[j + 1]) {
-                                int temp = arr[j];
-                                arr[j] = arr[j + 1];
-                                arr[j + 1] = temp;
+                    System.out.print("Nhập giá trị m: ");
+                    int m = scanner.nextInt();
+
+                    int[] newNumbers = new int[numbers.length + 1];
+                    for (int i = 0; i < numbers.length; i++) {
+                        newNumbers[i] = numbers[i];
+                    }
+                    newNumbers[numbers.length] = m;
+
+                    int n1 = newNumbers.length;
+                    for (int i = 0; i < n1 - 1; i++) {
+                        for (int j = 0; j < n1 - i - 1; j++) {
+                            if (newNumbers[j] < newNumbers[j + 1]) {
+                                int temp = newNumbers[j];
+                                newNumbers[j] = newNumbers[j + 1];
+                                newNumbers[j + 1] = temp;
                             }
                         }
                     }
-                    break;
 
+                    numbers = newNumbers;
+
+                    System.out.print("Mảng sau khi chèn m và sắp xếp giảm dần: ");
+                    for (int num : numbers) {
+                        System.out.print(num + " ");
+                    }
+                    System.out.println();
+                    break;
+           
                 case 9:
                     System.exit(0);
                     break;
